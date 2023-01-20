a38e8068d967160e63252518df69f553ae132a22
Sasha-Barisic <sashabarisic@gmail.com>
2023-01-20 22:07:41 +1100

    Finished adding the filters, fixed an issue going over the line, and added breaks in between sections

--- hcc_dashboard/hcc/components/filters.py
+++ hcc_dashboard/hcc/components/filters.py
@@ -1,8 +1,86 @@
 from dash import html
 import dash_bootstrap_components as dbc   
 
-filter_card = dbc.Card(
-        [   
+filter_card = html.Div([
+    dbc.Card(
+        [      
+        dbc.CardBody([    
+            dbc.Row([
+                dbc.Col([
+                    dbc.Label("Satellite"), 
+                    dbc.Checklist(
+                    options=[
+                        {"value": 1},
+                    ],
+                    value=1,
+                    id="satellite_switches",
+                    switch=True,
+                    inline=True,
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("Lymphovascular invasion"), 
+                    dbc.Checklist(
+                    options=[
+                        {"value": 1},
+                    ],
+                    value=1,
+                    id="lympho_vasc_switches",
+                    switch=True,
+                    inline=True,
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("Cirrhosis"), 
+                    dbc.Checklist(
+                    options=[
+                        {"value": 1},
+                    ],
+                    value=1,
+                    id="cirr_switches",
+                    switch=True,
+                    inline=True,
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("DM"), 
+                    dbc.Checklist(
+                    options=[
+                        {"value": 1},
+                    ],
+                    value=1,
+                    id="dm_switches",
+                    switch=True,
+                    inline=True,
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("IHD"), 
+                    dbc.Checklist(
+                    options=[
+                        {"value": 1},
+                    ],
+                    value=1,
+                    id="ihd_switches",
+                    switch=True,
+                    inline=True,
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("Prior TACE"), 
+                    dbc.Checklist(
+                    options=[
+                        {"value": 1},
+                    ],
+                    value=1,
+                    id="pr_tace_switches",
+                    switch=True,
+                    inline=True,
+                    ),
+                ]),
+            ]),
+            html.Hr(style={"border-width": "6px"}),
+            html.Br(),
             dbc.Row([
                 dbc.Col([
                     dbc.Label("Age"),
@@ -74,13 +152,106 @@ filter_card = dbc.Card(
                     id="radioitems-sex-input",
                     ),
                 ]),
+                dbc.Col([
+                    dbc.Label("INR"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">1.1", "value": 1},
+                        {"label": "<=1.1", "value": 2},
+                    ],
+                    value=1,
+                    id="radioitems-inr-input",
+                    ),
+                ]), 
             ]),
-          
-     
+            html.Hr(style={"border-width": "6px"}),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Label("Albumin"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">=35", "value": 1},
+                        {"label": "<35", "value": 2},
+                    ],
+                    value=2,
+                    id="radioitems-albumin-input",
+                    ),
+                ]), 
+                dbc.Col([
+                    dbc.Label("AFP"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">100", "value": 1},
+                        {"label": ">8 & <=100", "value": 2},
+                        {"label": "<=8", "value": 3},
+                    ],
+                    value=2,
+                    id="radioitems-afp-input",
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("HPVG"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">5", "value": 1},
+                        {"label": "<=5", "value": 2},
+                    ],
+                    value=2,
+                    id="radioitems-hpvg-input",
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("ALT"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">50", "value": 1},
+                        {"label": "<=50", "value": 2},
+                    ],
+                    value=1,
+                    id="radioitems-alt-input",
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("eGFR"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">=90", "value": 1},
+                        {"label": "<90", "value": 2},
+                    ],
+                    value=1,
+                    id="radioitems-egfr-input",
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("Bilirubin"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">20", "value": 1},
+                        {"label": "<=20", "value": 2},
+                    ],
+                    value=1,
+                    id="radioitems-bilirubin-input",
+                    ),
+                ]),
+                dbc.Col([
+                    dbc.Label("Size of Lesion (cm)"),
+                    dbc.RadioItems(
+                    options=[
+                        {"label": ">=5", "value": 1},
+                        {"label": "<5", "value": 2},
+                    ],
+                    value=1,
+                    id="radioitems-lesion_size-input",
+                    ),
+                ]),
+            ]),
+        ]), 
         ],
         style={
-            "margin-left": "5%",
-            "margin-right": "5%",
+            "margin-left": "3%",
+            "margin-right": "3%",
             "background": "transparent"
         },
-    )
+    ), 
+])
