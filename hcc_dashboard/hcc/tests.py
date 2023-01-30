d948c7f30a69efe3fd26fbca688b7a6072a0805b
Daniel Al Mouiee <d.almouiee@gmail.com>
2023-01-30 18:07:13 +1100

    Added tests and a pipeline file for pylint and tests validation

---
+++ hcc_dashboard/hcc/tests.py
@@ -0,0 +1,17 @@
+""" Module for testing dashboard
+"""
+from django.test import TestCase
+from .app import infer_model
+
+TEST_PATIENT_INPUT = [0, 4, 1, 2, 1, 1, 1, 1, 1, 0, 2, 1, 1, 1, 1, 1, 0, 1, 0, 1]
+
+
+class InferTestCase(TestCase):
+    """Class for testing the Model inference on the dashboard"""
+
+    def test_infer_model_callback(self):
+        """Function to test model inference callback with example patient"""
+        n_clicks = 1
+        output = infer_model(n_clicks, *TEST_PATIENT_INPUT)
+        assert output[0] == "13.34%"
+        assert output[1] == "75.26%"
--- hcc_dashboard/hcc_dashboard/settings.py
+++ hcc_dashboard/hcc_dashboard/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DATA_PATH = "data" if "DATA_PATH" in os.environ else "../data"
 
 # Application definition
 
