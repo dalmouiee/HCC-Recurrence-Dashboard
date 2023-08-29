c862794d04c5584f580c0a9552a168507a89bf8e
Jayen Ashar <jayenashar@users.noreply.github.com>
2023-08-29 14:35:31 +1000

    so the dashboard can be embedded on http://vafaeelab.com/HCC_Predictor.html

--- hcc_dashboard/hcc/views.py
+++ hcc_dashboard/hcc/views.py
@@ -2,10 +2,11 @@
     Django HCC dashboard views
 """
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 from .app import *  # pylint: disable=unused-wildcard-import,wildcard-import
 
-
+@xframe_options_exempt
 def index(request):
     """Function to direct to HCC dashboard view
 
